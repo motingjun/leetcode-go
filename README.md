5761189dfd63cfa728810a7ce260246996ae1754
motingjun <18501210524@163.com>
2021-09-27 18:56:05 +0800

    寻找两个正序数组的中位数

--- README.md
+++ README.md
@@ -10,3 +10,4 @@
 |0001|两数之和|[Go](https://github.com/motingjun/leetcode-go/tree/main/leetcode/0001.Two-Sum)|O(n)|O(n)|52.0%|简单||
 |0002|两数相加|[Go](https://github.com/motingjun/leetcode-go/tree/main/leetcode/0002.Add-Two-Numbers)|O(n)|O(1)|40.9%|中等||
 |0003|无重复字符的最长子串|[Go](https://github.com/motingjun/leetcode-go/tree/main/leetcode/0003.Longest-Substring-Without-Repeating-Characters)|O(n)|O(1)|38.0%|中等||
+|0004|寻找两个正序数组的中位数|[Go](https://github.com/motingjun/leetcode-go/tree/main/leetcode/0004.Median-of-Two-Sorted-Arrays)|||40.8%|困难||
---
+++ leetcode/0004.Median-of-Two-Sorted-Arrays/median_of_two_sorted_arrays_test.go
@@ -0,0 +1,63 @@
+package leetcode
+
+import (
+	"testing"
+)
+
+type question4 struct {
+	para4
+	ans4
+}
+
+// para 是参数
+// nums 代表正序数组
+type para4 struct {
+	nums1 []int
+	nums2 []int
+}
+
+// ans 是答案
+// one 代表第一个答案
+type ans4 struct {
+	one float64
+}
+
+func TestProblem4(t *testing.T) {
+
+	qs := []question4{
+
+		{
+			para4{[]int{1, 3}, []int{2}},
+			ans4{2.0},
+		},
+
+		{
+			para4{[]int{1, 2}, []int{3, 4}},
+			ans4{2.5},
+		},
+
+		{
+			para4{[]int{0, 0}, []int{0, 0}},
+			ans4{0.0},
+		},
+
+		{
+			para4{[]int{1}, []int{}},
+			ans4{1.0},
+		},
+
+		{
+			para4{[]int{}, []int{2}},
+			ans4{2.0},
+		},
+	}
+
+	t.Logf("-----------------------LeetCode Problem 4-----------------------\n")
+
+	for _, q := range qs {
+		_, p := q.ans4, q.para4
+		t.Logf("[input]:%v		[output]:%v\n", p, findMedianSortedArrays(p.nums1, p.nums2))
+	}
+
+	t.Logf("------------------------------END------------------------------\n")
+}
