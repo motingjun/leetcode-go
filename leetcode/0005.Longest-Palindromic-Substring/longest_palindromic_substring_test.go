1ac31513dd1b7ab70e4aab0f40a1c94cefc8bca1
motingjun <18501210524@163.com>
2021-09-28 19:29:51 +0800

    最长回文子串

---
+++ leetcode/0005.Longest-Palindromic-Substring/longest_palindromic_substring_test.go
@@ -0,0 +1,70 @@
+package leetcode
+
+import (
+	"testing"
+)
+
+type question5 struct {
+	para5
+	ans5
+}
+
+// para 是参数
+// s 代表字符串
+type para5 struct {
+	s string
+}
+
+// ans 是答案
+// one 代表第一个答案
+type ans5 struct {
+	one string
+}
+
+func TestProblem5(t *testing.T) {
+
+	qs := []question5{
+
+		{
+			para5{"babad"},
+			ans5{"bab"},
+		},
+
+		{
+			para5{"cbbd"},
+			ans5{"bb"},
+		},
+
+		{
+			para5{"a"},
+			ans5{"a"},
+		},
+
+		{
+			para5{"ac"},
+			ans5{"a"},
+		},
+
+		{
+			para5{"aa"},
+			ans5{"aa"},
+		},
+
+		{
+			para5{"ajgiljtperkvubjmdsefcylksrxtftqrehoitdgdtttswwttmfuvwgwrruuqmxttzsbmuhgfaoueumvbhajqsaxkkihjwevzzedizmrsmpxqavyryklbotwzngxscvyuqjkkaotitddlhhnutmotupwuwyltebtsdfssbwayuxrbgihmtphshdslktvsjadaykyjivbzhwujcdvzdxxfiixnzrmusqvwujjmxhbqbdpauacnzojnzxxgrkmupadfcsujkcwajsgintahwgbjnvjqubcxajdyyapposrkpqtpqfjcvbhlmwfutgognqxgaukpmdyaxghgoqkqnigcllachmwzrazwhpppmsodvxilrccfqgpkmdqhoorxpyjsrtbeeidsinpeyxxpsjnymxkouskyhenzgieybwkgzrhhrzgkwbyeigznehyksuokxmynjspxxyepnisdieebtrsjypxroohqdmkpgqfccrlixvdosmppphwzarzwmhcallcginqkqoghgxaydmpkuagxqngogtufwmlhbvcjfqptqpkrsoppayydjaxcbuqjvnjbgwhatnigsjawckjuscfdapumkrgxxznjozncauapdbqbhxmjjuwvqsumrznxiifxxdzvdcjuwhzbvijykyadajsvtklsdhshptmhigbrxuyawbssfdstbetlywuwputomtunhhlddtitoakkjquyvcsxgnzwtoblkyryvaqxpmsrmzidezzvewjhikkxasqjahbvmueuoafghumbszttxmquurrwgwvufmttwwstttdgdtioherqtftxrsklycfesdmjbuvkreptjligja"},
+			ans5{"ajgiljtperkvubjmdsefcylksrxtftqrehoitdgdtttswwttmfuvwgwrruuqmxttzsbmuhgfaoueumvbhajqsaxkkihjwevzzedizmrsmpxqavyryklbotwzngxscvyuqjkkaotitddlhhnutmotupwuwyltebtsdfssbwayuxrbgihmtphshdslktvsjadaykyjivbzhwujcdvzdxxfiixnzrmusqvwujjmxhbqbdpauacnzojnzxxgrkmupadfcsujkcwajsgintahwgbjnvjqubcxajdyyapposrkpqtpqfjcvbhlmwfutgognqxgaukpmdyaxghgoqkqnigcllachmwzrazwhpppmsodvxilrccfqgpkmdqhoorxpyjsrtbeeidsinpeyxxpsjnymxkouskyhenzgieybwkgzrhhrzgkwbyeigznehyksuokxmynjspxxyepnisdieebtrsjypxroohqdmkpgqfccrlixvdosmppphwzarzwmhcallcginqkqoghgxaydmpkuagxqngogtufwmlhbvcjfqptqpkrsoppayydjaxcbuqjvnjbgwhatnigsjawckjuscfdapumkrgxxznjozncauapdbqbhxmjjuwvqsumrznxiifxxdzvdcjuwhzbvijykyadajsvtklsdhshptmhigbrxuyawbssfdstbetlywuwputomtunhhlddtitoakkjquyvcsxgnzwtoblkyryvaqxpmsrmzidezzvewjhikkxasqjahbvmueuoafghumbszttxmquurrwgwvufmttwwstttdgdtioherqtftxrsklycfesdmjbuvkreptjligja"},
+		},
+	}
+
+	t.Logf("-----------------------LeetCode Problem 4-----------------------\n")
+
+	for _, q := range qs {
+		_, p := q.ans5, q.para5
+		t.Logf("[input]:%v		[output]:%v\n", p, longestPalindrome(p.s))
+		t.Logf("[input]:%v		[output]:%v\n", p, longestPalindrome1(p.s))
+		t.Logf("[input]:%v		[output]:%v\n", p, longestPalindrome2(p.s))
+		t.Logf("[input]:%v		[output]:%v\n", p, longestPalindrome3(p.s))
+	}
+
+	t.Logf("------------------------------END------------------------------\n")
+}
